66a433c7d39bd0f322d4891ebf7ef961b07c01be
Sanndricak <sanndricak@gmail.com>
2020-07-11 19:40:16 +0200

    first commit

---
+++ src/HelloworldWithJAVA.java
@@ -0,0 +1,19 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// komentari u JAVA se pisu nakon dve kose crte, ako je u jednom redu
+		/* ako je u vise redova
+		 onad se obelezava kombinacijom
+		 kose crte i zvezice*/
+		
+		System.out.println("Hello world!! This is my first project in JAVA!");
+		// println znaci da kursor prelayi u red ispod
+		System.out.println("Sto vise puta ponovis sintaksu, pre ces da je naucis!");
+		System.out.print("bey novog reda");
+		System.out.print("bey novog reda");
+		//primeti da kada je print prviput iskoriscen, ipak je otisao ispis u novi red tek drugi put nije
+	}
+
+}
